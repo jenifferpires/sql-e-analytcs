369b80a505ebf1eefb8af9aa11be9b8b43d07be5
Jeniffer Pires <jenifferpires@users.noreply.github.com>
2026-03-10 20:53:05 -0300

    feat: estrutura completa finalizada, bugs de ETL resolvidos e documentação visual incluída

--- case-loyalty-system/queries/analysis/top.clientes.sql
+++ case-loyalty-system/queries/analysis/top.clientes.sql
@@ -1,6 +1,7 @@
--- Top 10 clientes por valor total gasto
-SELECT c.email, SUM(i.quantidade * i.valor_unitario) AS total_gasto
-FROM tb_clientes c
-JOIN tb_vendas v ON c.id_cliente = v.id_cliente
-JOIN tb_itens_venda i ON v.id_transacao = i.id_transacao
-GROUP BY c.email ORDER BY total_gasto DESC LIMIT 10;
+SELECT 
+    `E-mail do Cliente`, 
+    COUNT(*) AS Qtd_Compras, 
+    SUM(`Total do Item`) AS Gasto_Total 
+FROM vw_relatorio_vendas 
+GROUP BY `E-mail do Cliente` 
+ORDER BY Gasto_Total DESC;
--- case-loyalty-system/queries/analysis/top_produtos_vendidos.sql
+++ case-loyalty-system/queries/analysis/top_produtos_vendidos.sql
@@ -1,11 +1,6 @@
-USE curso_sql;
-
--- Ranking de produtos por faturamento total
 SELECT 
-    p.nome_produto AS Produto,
-    SUM(i.quantidade * i.valor_unitario) AS Faturamento_Total
-FROM tb_itens_venda i
-JOIN tb_produtos p ON i.id_produto = p.id_product
-GROUP BY p.nome_produto
-ORDER BY Faturamento_Total DESC
-LIMIT 10;
+    Produto, 
+    SUM(`Total do Item`) AS Faturamento_Total 
+FROM vw_relatorio_vendas 
+GROUP BY Produto 
+ORDER BY Faturamento_Total DESC;
